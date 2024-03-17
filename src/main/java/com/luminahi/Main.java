82832429eba9c9bf59088fa1df36a672df61eda9
alex lima <delimaferreiraalex@gmail.com>
2024-03-17 13:59:42 -0300

    create folder for temp data

--- src/main/java/com/luminahi/Main.java
+++ src/main/java/com/luminahi/Main.java
@@ -1,5 +1,7 @@
 package com.luminahi;
 
+import java.io.File;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
@@ -7,12 +9,17 @@ import org.apache.catalina.startup.Tomcat;
 import com.luminahi.servlets.StaticServlet;
 
 public class Main {
+    
+    
     public static void main(String[] args) throws LifecycleException {
+        final String appDir = new File("").getAbsolutePath();
+    
         Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir(new File(appDir, "/target/tmp").getAbsolutePath());
         tomcat.setPort(8080);
         tomcat.getConnector();
 
-        Context context = tomcat.addContext("", null);
+        Context context = tomcat.addContext("", new File("").getAbsolutePath() + "/src/main/webapp");
 
         Tomcat.addServlet(context, "staticApp", new StaticServlet());
         context.addServletMappingDecoded("/", "staticApp");
